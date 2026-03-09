c2fceb07c6efc133a9a554315f8b03dc65152d77
David Bamson <bamsondavid1@gmail.com>
2026-03-09 12:27:33 +0100

    Added the "recents" feature

--- launcher.py
+++ launcher.py
@@ -1,7 +1,28 @@
 import tkinter
 import os
+import json
 from tkinter import filedialog as fd
+def load_data():
+    try:
+        with open("data.json", "r") as f:
+            return json.load(f)
+    except:
+        return{
+            "recent": [],
+            "favorites": []
+            }
+    
 
+
+def save_data(data):
+    try:
+        with open("data.json", "w") as f:
+            return json.dump(data, f)
+    except:
+        return "Try Again"
+        
+
+data = load_data()
 
 filepath = fd.askopenfilenames(
     title ="Select a File",
@@ -12,5 +33,8 @@ if filepath:
     print(f"Selected file: {filepath}")
     for path in filepath:
         os.startfile(path, "open")
+        data["recent"].append(path)
+        save_data(data)
 else:
     print("No file selected.")
+
