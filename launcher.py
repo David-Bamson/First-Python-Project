30881133ce4ac723bad0bf5b39be9e2597e1b43f
David Bamson <bamsondavid1@gmail.com>
2026-03-09 11:55:08 +0100

    Added the multiple file selection feature

--- launcher.py
+++ launcher.py
@@ -3,15 +3,14 @@ import os
 from tkinter import filedialog as fd
 
 
-filepath = fd.askopenfilename(
+filepath = fd.askopenfilenames(
     title ="Select a File",
     filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
 )
 
 if filepath:
     print(f"Selected file: {filepath}")
+    for path in filepath:
+        os.startfile(path, "open")
 else:
     print("No file selected.")
-
-
-os.startfile(filepath, "open")
